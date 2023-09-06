7d7986798f7af7f1f23df58ca11392176a56573b
Microzzx <janekitpk@gmail.com>
2023-09-06 21:58:39 +0700

    add songDetails and RelatedSongs

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,34 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
-);
+import { Link } from "react-router-dom";
 
+const DetailsHeader = ({ artistId, songData }) => {
+  const songAttribute = songData?.data[0]?.attributes;
+  return (
+    <div className="relative w-full flex flex-col">
+      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+      <div className="absolute inset-0 flex items-center ml-5">
+        <img
+          src={songAttribute?.artwork?.url
+            .replace("{w}", "500")
+            .replace("{h}", "500")}
+          className="sm:w-28 w-28 sm:h-28 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
+        />
+        <div className="ml-5">
+          <p className="text-white text-xl sm:text-3xl text-bold ">
+            {songAttribute?.albumName}
+          </p>
+
+          <Link to={`/artists/${artistId}`}>
+            <p className=" text-base text-gray-300 mt-2">
+              {songAttribute?.artistName}
+            </p>
+          </Link>
+
+          <p className="text-base text-gray-300 mt-2">
+            {songAttribute?.genreNames[0]}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default DetailsHeader;
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,65 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import {
+  useGetSongDetailsQuery,
+  useGetTopSongsQuery,
+} from "../redux/services/shazam";
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songid } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: songData, isFetching: isFetchingSongDetails } =
+    useGetSongDetailsQuery({ songid });
+  console.log(songData);
+  const artistId = songData?.data[0]?.relationships?.artists?.data[0]?.id;
+  const {
+    data,
+    isFetching: isFetchingTopSongs,
+    error,
+  } = useGetTopSongsQuery({ artistId });
+  console.log("4:", data);
+
+  //const topSongData = Object.values(data);
+
+  const handlePause = () => {
+    dispatch(playPause(false));
+  };
+  const handlePlay = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  if (isFetchingSongDetails || isFetchingTopSongs) {
+    return <Loader title="searching"></Loader>;
+  }
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId={artistId} songData={songData} />
+      <div className="mb-10 ml-5 mt-5">
+        <h2 className="text-white text-3xl font-bold">Lyrics</h2>
+        <div className="mt-5">
+          {songData?.lyric ? (
+            <p className="text-gray-300 text-base mt-1">Lyrics.</p>
+          ) : (
+            <p className="text-gray-300 text-base mt-1">No Lyrics Found. </p>
+          )}
+        </div>
+      </div>
+
+      <RelatedSongs
+        data={data}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePause={handlePause}
+        handlePlay={handlePlay}
+        artistId={artistId}
+      />
+    </div>
+  );
+};
 
 export default SongDetails;
